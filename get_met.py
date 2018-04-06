245a70cedded27d72570cfd5206003c5579f4d02
Bum Mook Oh <bmo5@uw.edu>
2018-04-05 22:44:19 -0700

    fix bug

--- get_met.py
+++ get_met.py
@@ -1,12 +1,19 @@
 import csv
+import ssl
 import time 
+import random
 import urllib2
 import requests
+import validators
 from BeautifulSoup import BeautifulSoup
 
+
+# original    https://images.metmuseum.org/CRDImages/is/original/sf08-256-270a.jpg
+# small image https://images.metmuseum.org/CRDImages/is/web-large/sf08-256-270a.jpg
+
 def get_webpage(web_page):
-    html_page = urllib2.urlopen(web_page)
-    return html_page
+    html_page = requests.get(web_page)
+    return html_page.text
 
 def parse_and_get_image(html_page):
     soup = BeautifulSoup(html_page)
@@ -15,23 +22,45 @@ def parse_and_get_image(html_page):
     return img_src
 
 def save_image_as(img_src, new_img_name):
-    r = requests.get(img_src, allow_redirects=True)
+    img_src = img_src.replace('web-large', 'original').replace("https://","http://")
+    r = requests.get(img_src)
+    # try again in smaller quality if cannot find
+    if r.status_code != 200:
+        img_src = img_src.replace('original', 'web-large')
+        r = requests.get(img_src, verify=False)
     open("images/" + new_img_name + ".jpg", 'wb').write(r.content)
 
 def read_meta_csv(file_name):
+    images_found = 0
+    images_not_found = 0
     with open(file_name, "rb") as f:
         reader = csv.reader(f, delimiter=',')
         next(reader)
         for row in reader:
             try:
-                html_page = get_webpage(row[40])
-                img_src = parse_and_get_image(html_page)
-                if img_src:
-                    save_image_as(img_src, row)
-            except:
-                print "Some error occured"
-            time.sleep(1.2)
-        
+                for col in row:
+                    if isValidUrl(col):
+                        html_page = get_webpage(col.replace("http://","https://"))
+                        img_src = parse_and_get_image(html_page)
+                        if img_src:
+                            if img_src != '/content/img/placeholders/noimage534x534.png':
+                                save_image_as(img_src, row[3])
+                                images_found += 1
+                            else :
+                                images_not_found += 1
+                        break
+
+            except Exception as e: 
+                print str(e)
+                images_not_found += 1
+            time.sleep(2.8)
+
+    print str(images_found) + " images found"
+    print str(images_not_found) + " images not found"
+
+def isValidUrl(input_string):
+    return validators.url(input_string)
+
 def main():
     file_name = "MetObjects.csv"
     read_meta_csv(file_name)
